328ce8a28fa925d7c4967e23fe08e57377f69998
Toni <juniortoni16@gmail.com>
2022-09-15 13:31:23 +0200

    Ejemplos 3 de Js

---
+++ js/main.js
@@ -0,0 +1,101 @@
+
+// function Sumar (numero1, numero2) {
+
+//     return numero1 + numero2;
+// };
+
+// let numero1 = parseInt(prompt("Introduce un número"));
+// let numero2 = parseInt(prompt("Introduce otro número"));
+
+// console.log(Sumar(numero1,numero2));
+
+// let numeroFavorito = 3;
+
+// numeroFavorito = 6;
+
+// numeroFavorito += 5;
+// numeroFavorito -= 3;
+
+// numeroFavorito **= 2;
+
+// console.log(numeroFavorito);
+
+// let pelicula = "Matrix";
+
+// if(pelicula == "Blade Runner"){
+
+//     console.log("Es la pelicula favorita de Christian");
+
+// }else{
+
+//     console.log("No es la pelicula favorita de Christian");
+// };
+
+// let numeroPaula = 22;
+
+// if(numeroPaula === "22"){
+//     console.log("Soy el mismo");
+// }else {
+//     console.log("No soy el mismo");
+// }
+
+// let operacion = prompt("Dime que operacion quieres realizar").toLowerCase();
+
+// let numero1 = parseInt(prompt("Dime el primer número"));
+
+// let numero2 = parseInt(prompt("Dime el segundo número"));
+
+// function Operar(operacion, numero1, numero2){
+
+//     //Analizo el tipo de operación que se va a realizar
+
+//     if(operacion == "Sumar"){
+
+//         return numero1 + numero2;
+
+//     } else if (operacion == "Restar"){
+
+//         return numero1 - numero2;
+
+//     } else if (operacion == "Multiplicar"){
+
+//         return numero1 * numero2;
+
+//     } else if (operacion == "Dividir"){
+//         return numero1 / numero2;
+//     }
+// }
+
+// switch(operacion){
+
+//     case 'sumar': 
+//         console.log(numero1 + numero2);
+//     break;
+
+//     case 'restar': 
+//         console.log(numero1 - numero2);
+//     break;
+
+//     case 'multiplicar': 
+//         console.log(numero1 * numero2);
+//     break;
+
+//     case 'dividir': 
+//         console.log(numero1 / numero2);
+//     break;
+
+//     default:
+//         console.log("No sabes escribir");
+
+//     break;
+
+
+// }
+
+// let aguacate = false;
+
+// if(!aguacate){
+//     console.log("Nos arruinamos");
+// }else{
+//     console.log("Comemos patata");
+// }if
